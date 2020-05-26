4feff0fbd885dcf965529ef5e5dcabde705c84c4
Rocio Avila <rocio.avila@northwestern.com>
2020-05-26 01:39:02 -0500

    Insert Lowercase,Uppercase,Number,Symbols Settings

--- Develop/script.js
+++ Develop/script.js
@@ -3,23 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//Password Values
-let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+";
-
-let password = ""
-
-// loop for generating characters 
-for(var i=0; i< values.length; i++){
-  password = pasword + values.charAt(Math.floor(Math.random()))
-  console.log(values[i])
-}
