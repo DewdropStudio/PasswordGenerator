3907115df456fb7b0e405fd7510695e8b0cff860
Rocio Avila <rocio.avila@northwestern.com>
2020-05-26 23:04:45 -0500

    Added Password image

--- Develop/script.js
+++ Develop/script.js
@@ -1,109 +1,88 @@
+const characterTotalNumber = document.getElementById("characterTotalNumber")
+
+// const number = document.getElementById("number")
+
+// length.addEventListener("input", synclength)
+const includeLowercaseElement = document.getElementById("includeLowercase")
+
+const includeUppercaseElement = document.getElementById("includeUppercase")
+
+const includeNumbersElement = document.getElementById("includeNumbers")
+
+const includeSymbolsElement = document.getElementById("includeSymbols")
+
+const form = document.getElementById("passwordGeneratorForm")
+
+
+// Original
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   // var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-// }
-// Add event listener to generate buttonwritePassword 
-// generateBtn.addEventListener("click", writePassword,() => {
-//   var length lengthEl.value;
+//   Original End
 
-//   resultEl.innerText = generatePassword(),
-//   console.log(length)
-// });
 
-// //Password Values
-// let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+";
+const UPPERCASE_CHAR_CODES =arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES =arrayFromLowToHigh(97, 122)
 
-// let password = ""
+const NUMBERS_CHAR_CODES =arrayFromLowToHigh(48, 57)
 
-// loop for generating characters 
-for(var i=0; i< length; i++){
-  password = password + values.charAt(Math.floor(Math.random()))
-  console.log(values[i])
+const SYMBOLS_CHAR_CODES =arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64))
+console.log(SYMBOLS_CHAR_CODE())
+
+characterTotalNumber.addEventListener("input", syncnumber)
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const characterTotalNumber = characterTotalNumber.value
+
+    const includeLowercase = includeLowercaseElement.checked
+
+    const includeUppercase = includeUppercaseElement.checked
+
+    const includeNumbers = includeNumbersElement.checked
+
+    const includeSymbols = includeSymbolsElement.checked
+
+    const password = generatePassword (characterTotalNumber, includeLowercase, includeUppercase, includeNumbers, includeSymbols)
+    
+})
+
+
+function generatePassword(characterTotalNumber, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+    // String.fromCharCode(65)
+    let charCodes = LOWERCASE_CHAR_CODES
+    if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+    if (includeNumbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
+    if (includeSymbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES)
+
+    const passwordCharacters = []
+for (let i = 0; i < characterTotalNumber; i++) {
+    const characterCode = charCodes[Math.floor(Math.random()* charCodes.length)]
+passwordCharacters.push(String.fromCharCode(characterCode))
+}
+return passwordCharacters.join("")
+}
+
+function arrayFromLowToHigh(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++ ) {
+        array.push(i)
+    }
+    return array
 }
 
 
-// DOM Element
-var lowercaseEl = document.getElementById("lowercase");
 
-var uppercaseEl = document.getElementById("uppercase");
-
-var symbolsEl = document.getElementById("symbols");
-
-var numbersEl = document.getElementById("numbers");
-
-var generateEl = document.getElementById("generate");
-var lengthEl = document.getElementById("length");
-
-var resultEl = document.getElementById("result");
-
-
-// Genrate click
-generateEl.addEventListener(".click", writepassword, () => {
-  const length = lengthEl.value;
-  const hasLower = lowercaseEl.checked;
-  const hasUpper = uppercaseEl.checked;
-  const hasNumber = numbersEl.checked;
-  const hasSymbol = symbolsEl.checked;
-  
-  console.log(hasLower, hasUpper, hasNumber, hasSymbol);
-
-resultEl.innerText = generatePassword(length,hasLower, hasUpper, hasNumber, hasSymbol);
-});
-
-// Generate Password Function
-function generatePassword(lower, upper, number, symbol, length) {
-let generatedPassword = ""
-const typesCount = lower + upper + number + symbol;
-const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
-console.log("typesCount:", typesCount);
-
-if (typesCount ===0){
-  return "";
-  
-//   // loop
-//   for (let i = 0; i < length; i += typesCount ) {
-// typesArr.forEach(type => {
-//   const funcName = Object.keys(type)[0];
-
-//   generatedPassword += randomFunc[funcName]();
-//   console.log("funcName:", funcName)
-// })
-//   }
-// }
+function syncnumber(e) {
+    const value = e.target.value
+    characterTotalNumber.value = value
 }
-
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  symbol: getRandomSymbol,
-  number: getRandomNumber,
-  
-};
-
-
-// Functions
-function getRandomLower () {
-return String.fromCharCode(Math.floor(Math.random() *26)+97);
 }
-
-function getRandomUpper () {
-  return String.fromCharCode(Math.floor(Math.random()*26)+65);
-}
-
-function getRandomSymbol () {
-  const symbols="!@#$%^&*()_+";
-  return symbols[Math.floor(Math.random()*symbols.length)];
-}
-
-function getRandomNumber () {
-  return String.fromCharCode(Math.floor(Math.random()*10)+48)
-};
-} ; }
